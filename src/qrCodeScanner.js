eedd368257c2a083a68a3381300b3ebb1eb21223
Brandon Billstrand <brandon@BrandonJ.Billstrand>
2020-07-28 00:24:12 -0500

    First Commit

--- src/qrCodeScanner.js
+++ src/qrCodeScanner.js
@@ -5,34 +5,34 @@ const canvas = canvasElement.getContext("2d");
 const qrResult = document.getElementById("qr-result");
 const outputData = document.getElementById("outputData");
 const btnScanQR = document.getElementById("btn-scan-qr");
+var data;
+var data2;
 let scanning = false;
 btnScanQR.hidden=true;
 
-
- 
  qrcode.callback = res => {
   if (res) {
-    outputData.innerText = res;
-    var data = res;
-    var destination = document.getElementById("iframe").contentWindow;
-    destination.postMessage(data,'*');
-    scanning = true;
-    // video.srcObject.getTracks().forEach(track => {
-    //   track.stop();
-    // });
-    qrResult.hidden = true;
-    canvasElement.hidden = false;
-    btnScanQR.hidden = true;
+    data = res;
+    console.log("Data1  "+data);
+    sendTestId();
+    scanning = true;  
     setTimeout(autoRefresh, 1000)
-    
-
-  }
-};
+     }
+    };
 
 function autoRefresh () {
   location.reload();
 }
 
+function sendSubmissionId () {
+  var destination1 = document.getElementById("iframe").contentWindow;
+    destination1.postMessage(data,'*');
+}
+
+function sendTestId () {
+  var destination2 = document.getElementById("iframe").contentWindow;
+    destination2.postMessage(data,'*');
+}
 
 window.onload = function() {
   navigator.mediaDevices
